3bfe17dc19164635e730318e6ad2c15785d4d0e8
Christopher Major <chriscjor100@gmail.com>
2020-08-01 21:41:59 -0400

    Implemented memory perception tick, update, and door memory update, door now has a c++ class for extra los checks on the center of the door and the door handle, added guard behaviour for closing opened doors

---
+++ Source/ProjectSpy/Door.cpp
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Door.h"
+#include "Components/StaticMeshComponent.h"
+#include "Engine/SkeletalMeshSocket.h"
+
+FVector ADoor::GetSocketLocation_Implementation(FName Socket) const
+{
+    return FVector();
+}
+
+
+//Raycasts to the root actor component first, if it cannot hit that it checks DoorHandle and Center socket for extra los checks
+bool ADoor::CanBeSeenFrom(const FVector& ObserverLocation, FVector& OutSeenLocation, int32& NumberOfLoSChecksPerformed,
+                          float& OutSightStrength, const AActor* IgnoreActor) const
+{
+    //Shorthand for FName value
+    static const FName NAME_AILineOfSight = FName(TEXT("SocketLineOfSight"));
+
+    FHitResult HitResult;
+
+    //Raycast to actor location
+    const bool bHit = GetWorld()->LineTraceSingleByChannel(HitResult, ObserverLocation, GetActorLocation() , ECollisionChannel(ECC_Visibility), FCollisionQueryParams(NAME_AILineOfSight, true, IgnoreActor));
+
+    NumberOfLoSChecksPerformed++;
+
+    //Return true if raycast hit actor
+    if (bHit == false || (HitResult.Actor.IsValid() && HitResult.Actor->IsOwnedBy(this)))
+    {
+        OutSeenLocation = GetActorLocation();
+        OutSightStrength = 1;
+
+        return true;
+    }
+
+    //Array of sockets to check
+    TArray<FName> Sockets;
+    Sockets.Add("DoorHandle");
+    Sockets.Add("Center");
+
+    //Iterate through all sockets to check LOS
+    for (int i = 0; i < Sockets.Num(); i++)
+    {
+        //Shorthand for FVector
+        FVector SocketLocation = GetSocketLocation(Sockets[i]);
+
+        //Raycast to socket location
+        const bool bHitSocket = GetWorld()->LineTraceSingleByChannel(HitResult, ObserverLocation, SocketLocation, ECollisionChannel(ECC_Visibility), FCollisionQueryParams(NAME_AILineOfSight, true, IgnoreActor));
+
+        NumberOfLoSChecksPerformed++;
+
+        //Return true if raycast hit actor
+        if (bHitSocket == false || (HitResult.Actor.IsValid() && HitResult.Actor->IsOwnedBy(this))) {
+            OutSeenLocation = SocketLocation;
+            OutSightStrength = 1;
+
+            return true;
+        }
+    }
+
+    //Return false if nothing was hit
+    OutSightStrength = 0;
+    return false;
+}
---
+++ Source/ProjectSpy/Door.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Perception/AISightTargetInterface.h"
+#include "Navigation/NavLinkProxy.h"
+#include "Door.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSPY_API ADoor : public ANavLinkProxy, public IAISightTargetInterface
+{
+	GENERATED_BODY()
+
+public:
+	//Blueprint function to set socket locations
+	UFUNCTION(BlueprintNativeEvent)
+	FVector GetSocketLocation(FName Socket) const;
+	
+	// Overide base function to add socket locations to raycast
+	UFUNCTION(BlueprintCallable)
+    virtual bool CanBeSeenFrom(const FVector& ObserverLocation, FVector& OutSeenLocation, int32& NumberOfLoSChecksPerformed, float& OutSightStrength, const AActor* IgnoreActor = NULL) const override;
+};
