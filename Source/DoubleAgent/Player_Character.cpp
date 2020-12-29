20b20288d76f2921d18baf1a00c1175bd1900625
Christopher Major <chriscjor100@gmail.com>
2020-12-28 23:50:28 -0500

    Replaced roomsafe with tresspassing, added illegalaction

--- Source/DoubleAgent/Player_Character.cpp
+++ Source/DoubleAgent/Player_Character.cpp
@@ -2,7 +2,38 @@
 
 
 #include "Player_Character.h"
+#include "AI/RoomVolume.h"
 
 APlayer_Character::APlayer_Character()
 {
+	//Register overlap events
+	OnActorBeginOverlap.AddDynamic(this, &APlayer_Character::CheckRooms);
+	OnActorEndOverlap.AddDynamic(this, &APlayer_Character::CheckRooms);
+}
+
+void APlayer_Character::CheckRooms(class AActor* OverlappedActor, class AActor* OtherActor)
+{
+	//If server
+	if (UKismetSystemLibrary::IsServer(GetWorld()))
+	{
+		//Get overlapping room volumes
+		TArray<AActor*> RoomVolumes;
+		GetOverlappingActors(RoomVolumes, ARoomVolume::StaticClass());
+
+		bool Temp = true;		
+		for (int i = 0; i < RoomVolumes.Num(); i++)
+		{
+			if (Cast<ARoomVolume>(RoomVolumes[i])->bPublic)
+				Temp = false;
+		}
+
+		bTresspassing = Temp;
+	}
+}
+
+void APlayer_Character::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	CheckRooms(nullptr, nullptr);
 }
