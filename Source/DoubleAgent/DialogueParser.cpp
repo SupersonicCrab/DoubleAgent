144cd13a8f44d780149f8f45a0b020df163975e2
Eric Mazza <dever0x@tuta.io>
2021-04-08 16:54:04 -0400

    (hopefully) fixes the dialogue packaging issue

--- Source/DoubleAgent/DialogueParser.cpp
+++ Source/DoubleAgent/DialogueParser.cpp
@@ -15,7 +15,7 @@ FDialogueInfo DialogueParser::GetDialogueInfo(FString DialogueKey){
     //Declare an empty string to hold the incoming data
     FString DialogueScript;
     //Load the text from the designated path to the designated string
-    FFileHelper::LoadFileToString(DialogueScript, *(FPaths::ProjectDir() + "Dialogue.txt"));
+    FFileHelper::LoadFileToString(DialogueScript, *(FPaths::ProjectDir() + "Content/Dialogue/Dialogue.txt"));
     //Find the line for the dialogue information by using the DialogueKey
     int KeyStart = DialogueScript.Find(DialogueKey);
     int Start = KeyStart + (DialogueKey.Len() + 1);
@@ -34,18 +34,8 @@ FDialogueInfo DialogueParser::GetDialogueInfo(FString DialogueKey){
     //Gather the dialogue message from the start and end point
     temp.Message = DialogueScript.Mid(Start, EndCount);
 
-    //The next substring is going to start where the previous one ended
-    Start = End + 1;
-    //The end is now going to be 2 digits after the start
-    End = Start + 2;
-    EndCount = 2;
-    //Because our message length is a float, we need to convert our FString into an std::string and then into a float in order to store it
-    FString TempLength = DialogueScript.Mid(Start, EndCount);
-    std::string STDTempLength(TCHAR_TO_UTF8(*TempLength));
-    temp.MessageLength = std::stof(STDTempLength);
-
     //The next substring is going to start 2 digits after the previous one ended
-    Start = End + 2;
+    Start = End + 5;
 
     //Iterate through each character until the desired character has been found
     for(int32 i = Start; i < DialogueScript.Len(); i++){
